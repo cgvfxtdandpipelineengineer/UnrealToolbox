629922c9716a7ba3e943d0d5df84cd2b4192bee0
Satish Goda <satishgoda@live.com>
2018-05-15 09:51:40 +0800

    Create sys.path.py

---
+++ py/sys.path.py
@@ -0,0 +1,26 @@
+LogPython: import sys
+LogPython: print "\n".join(sys.path)
+"""
+LogPython: C:\Program Files\Epic Games\UE_4.19\Engine\Source\ThirdParty\Python\Win64\python27.zip
+C:\Program Files\Epic Games\UE_4.19\Engine\Source\ThirdParty\Python\Win64\DLLs
+C:\Program Files\Epic Games\UE_4.19\Engine\Source\ThirdParty\Python\Win64\lib
+C:\Program Files\Epic Games\UE_4.19\Engine\Source\ThirdParty\Python\Win64\lib\plat-win
+C:\Program Files\Epic Games\UE_4.19\Engine\Source\ThirdParty\Python\Win64\lib\lib-tk
+C:\Program Files\Epic Games\UE_4.19\Engine\Binaries\Win64
+C:\Program Files\Epic Games\UE_4.19\Engine\Source\ThirdParty\Python\Win64
+C:\Program Files\Epic Games\UE_4.19\Engine\Source\ThirdParty\Python\Win64\lib\site-packages
+//isivip.anim.odw.com.cn/winsys/ad_profiles/sgoda/Documents/UnrealEngine/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Content/Python
+C:/Users/sgoda/Downloads/Unreal Projects/test1/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Plugins/2D/Paper2D/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Plugins/Compositing/LensDistortion/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Plugins/Editor/CryptoKeys/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Plugins/Enterprise/DatasmithContent/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Plugins/Experimental/PythonScriptPlugin/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Plugins/Media/MediaCompositing/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Plugins/Runtime/AppleARKit/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Plugins/Runtime/MixedRealityFramework/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Plugins/Runtime/Oculus/OculusVR/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Engine/Plugins/Runtime/Steam/SteamVR/Content/Python
+C:/Program Files/Epic Games/UE_4.19/Enterprise/Plugins/Experimental/EditorScriptingUtilities/Content/Python
+"""
